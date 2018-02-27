c4e9c9587f68d6a8f3595f9df1add4076c5e8a63
Lewis Gaul <lewis@scphillips.com>
2018-02-27 23:49:45 +0000

    Initial structure

--- __main__.py
+++ __main__.py
@@ -1,1 +1,7 @@
+import sys
+from os.path import join
+sys.path[0] = join(sys.path[0], 'src')
 
+from main import main
+
+main()
---
+++ src/main.py
@@ -0,0 +1,8 @@
+
+
+def main():
+    print("Running...")
+
+
+if __name__ == '__main__':
+    main()
