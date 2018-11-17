ad41c5ed51f3940bae45c920e6601383f24ec528
Lewis Gaul <lewis@scphillips.com>
2018-11-17 15:47:06 +0000

    Add game engine test outline

---
+++ tests/mut/game_engine_test.py
@@ -0,0 +1,22 @@
+"""
+game_engine_test.py - Test the game engine module
+
+October 2018, Lewis Gaul
+
+Uses pytest - simply run 'python -m pytest tests/ [-k game_engine_test]' from
+the root directory.
+"""
+
+
+import pytest
+
+from minegauler.shared.internal_types import *
+# from minegauler.backend.game_engine import 
+
+
+class TestGameEngine :
+    # --------------------------------------------------------------------------
+    # Test cases
+    # --------------------------------------------------------------------------
+    def test_dummy(self):
+        pass
