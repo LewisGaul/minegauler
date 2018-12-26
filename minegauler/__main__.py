400b757cec3b40e1f70cb0ca6e90a8a0f9afc363
Lewis Gaul <legaul@cisco.com>
2018-12-26 15:53:43 +0000

    Add logging to file

--- minegauler/__main__.py
+++ minegauler/__main__.py
@@ -5,10 +5,15 @@ December 2018, Lewis Gaul
 """
 
 
+import logging
 import sys
 
 from minegauler.backend import Controller, GameOptsStruct
 from minegauler.frontend import run
+
+
+logging.basicConfig(filename='runtime.log', level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 ctrlr = Controller(GameOptsStruct())
