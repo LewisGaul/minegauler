69ed474747fdf045733e0ba7b09a77540ad1fc53
Lewis Gaul <legaul@cisco.com>
2019-08-20 22:30:58 +0100

    Improve the cell contents types

--- minegauler/shared/internal_types.py
+++ minegauler/shared/internal_types.py
@@ -18,90 +18,90 @@ GameFlagMode (Enum)
     The possible flagging modes for a game.
 """
 
-
 import enum
+import functools
 
 
 # ------------------------------------------------------------------------------
 # Cell contents types
 # ------------------------------------------------------------------------------
 
-class CellContentsType(int):
+class _NumericCellContentsMixin:
     """
-    Abstract base class for contents of a minesweeper board cell.
+    A mixin for numeric cell contents types, allowing adding and subtracting integers.
     """
-    def __new__(cls, num):
-        if cls == CellContentsType:
-            raise TypeError(
-                f"{type(cls)} should be used as a base class and not "
-                 "instantiated directly")
-        return super().__new__(cls, num)
-    def __str__(self):
-        return repr(self)
-    def __eq__(self, obj):
-        if not isinstance(obj, type(self)):
-            return False
-        else:
-            return super().__eq__(obj)
-    def __ne__(self, obj):
-        return not (self == obj)
-    def __hash__(self):
-        return super().__hash__() ^ hash(type(self))
+
+    def __init__(self, num):
+        if not isinstance(num, int):
+            raise TypeError("Number should be an integer")
+        self.num = num
+
+    def __repr__(self):
+        return self.char + str(self.num)
+
     def __add__(self, obj):
         if type(obj) is not int:
-            raise ValueError("Can only add integers to cell contents types")
+            raise TypeError("Can only add integers to cell contents types")
         else:
-            return self.__class__(super().__add__(obj))
+            return self.__class__(self.num + obj)
+
     def __sub__(self, obj):
-        if type(obj) is int:
-            raise ValueError(
-                "Can only subtract integers from cell contents types")
+        if type(obj) is not int:
+            raise TypeError("Can only subtract integers from cell contents types")
         else:
-            return self.__class__(super().__add__(obj))
+            return self.__class__(self.num - obj)
+
+
+class CellContentsType:
+    """Abstract base class for contents of a minesweeper board cell."""
+
+    char = None
+
+    @functools.lru_cache(maxsize=None)
+    def __new__(cls, *args):
+        if cls == CellContentsType:
+            raise TypeError("Base class should not be instantiated")
+        return super().__new__(cls)
+
+    def __str__(self):
+        return repr(self)
+
+    def __repr__(self):
+        return self.char
 
 
 class CellUnclicked(CellContentsType):
-    """
-    Unclicked cell on a minesweeper board.
-    """
+    """Unclicked cell on a minesweeper board."""
+
     char = '#'
-    def __new__(cls):
-        return super().__new__(cls, 0)
-    def __repr__(self):
-        return self.char
-    def __add__(self, obj):
-        raise TypeError("Cannot add to unclicked cell")
-    def __sub__(self, obj):
-        raise TypeError("Cannot subtract from unclicked cell")
 
 
-class CellNum(CellContentsType):
-    """
-    Number shown in a cell on a minesweeper board.
-    """
-    def __new__(cls, num):
+class CellNum(_NumericCellContentsMixin, CellContentsType):
+    """Number shown in a cell on a minesweeper board."""
+
+    char = ''
+
+    def __init__(self, num):
+        super().__init__(num)
         if num < 0:
             raise ValueError("Cell value cannot be negative")
-        else:
-            return super().__new__(cls, num)
 
 
-class CellMineType(CellContentsType):
-    """
-    Abstract base class for the number of a mine type in a cell.
-    """
-    char = None
+class CellMineType(_NumericCellContentsMixin, CellContentsType):
+    """Abstract base class for the number of a mine type in a cell."""
+
     def __new__(cls, num=1):
         if cls == CellMineType:
             raise TypeError(
                 f"{type(cls)} should be used as a base class and not "
                 "instantiated directly")
+        return super().__new__(cls, num)
+
+    def __init__(self, num=1):
+        super().__init__(num)
         if num < 1:
-            raise ValueError("Mine-like type intended for 1 or more mines")
-        else:
-            return super().__new__(cls, num)
-    def __repr__(self):
-        return self.char + str(int(self))
+            raise ValueError("Mine-type cell contents must represent one or more mines")
+
     @staticmethod
     def get_class_from_char(char):
         """
@@ -120,29 +120,24 @@ class CellMineType(CellContentsType):
                 return cls
 
 class CellMine(CellMineType):
-    """
-    Number of mines in a cell shown on a minesweeper board.
-    """
+    """Number of mines in a cell shown on a minesweeper board."""
+
     char = 'M'
 
 class CellHitMine(CellMineType):
-    """
-    Number of hit mines in a cell shown on a minesweeper board.
-    """
+    """Number of hit mines in a cell shown on a minesweeper board."""
+
     char = '!'
 
 class CellFlag(CellMineType):
-    """
-    Number of flags in a cell shown on a minesweeper board.
-    """
+    """Number of flags in a cell shown on a minesweeper board."""
+
     char = 'F'
 
 class CellWrongFlag(CellMineType):
-    """
-    Number of incorrect flags in a cell shown on a minesweeper board.
-    """
-    char = 'X'
+    """Number of incorrect flags in a cell shown on a minesweeper board."""
 
+    char = 'X'
 
 
 # ------------------------------------------------------------------------------
