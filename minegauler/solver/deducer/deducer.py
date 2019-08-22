7c6ed784dd64170984b2ce356c0508331a493b12
Lewis Gaul <legaul@cisco.com>
2019-08-22 23:14:54 +0100

    Add some basic deduction

--- minegauler/solver/deducer/deducer.py
+++ minegauler/solver/deducer/deducer.py
@@ -5,16 +5,16 @@ August 2019, Lewis Gaul
 """
 
 from minegauler.backend.utils import Board
-from minegauler.shared.internal_types import CellContentsType
+from minegauler.shared.internal_types import CellContentsType, CellNum, CellUnclicked, CellMineType
 
 
-class CellSafe(CellContentsType):
+class CellSafe(CellUnclicked):
     """Indication that a cell is safe to click."""
 
     char = 'S'
 
 
-class CellUnsafe(CellContentsType):
+class CellUnsafe(CellUnclicked):
     """Indication that a cell contains a mine."""
 
     char = 'U'
@@ -30,5 +30,33 @@ def deduce(board: Board) -> Board:
     :return:
         The board containing deductions.
     """
-    pass
+    deduced_board = board.copy()
+    _deduce_by_one_neighbour(deduced_board)
+    return deduced_board
+
+
+def _deduce_by_one_neighbour(board: Board) -> None:
+    """
+    Make deductions using numbers that are only adjacent to one clickable cell.
+
+    :param board:
+        The board to mutate.
+    """
+    for coord in board.all_coords:
+        val = board[coord]
+        if not isinstance(val, CellNum):
+            continue
+        nbrs = board.get_nbrs(coord)
+        unclicked_nbrs = [
+            c for c in nbrs if type(board[c], CellUnclicked)
+        ]
+        nbr_mines = board[coord].num - sum([board[c].num for c in nbrs if isinstance(board[c], CellMineType)])
+        if len(unclicked_nbrs) == nbr_mines:
+            for c in unclicked_nbrs:
+                board[c] = CellUnsafe()
+        elif nbr_mines == 0:
+            for c in unclicked_nbrs:
+                board[c] = CellSafe()
+        # else:
+        #     raise RuntimeError("Too many flags around cell {}".format(coord))
 
