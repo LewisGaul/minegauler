7136f67c32bcdcaf0be88c1d7ff134265b810a3b
Felix Gaul <f.gaul@googlemail.com>
2019-12-25 01:21:32 +0000

    Sort out test imports (but still 2 failing..?)

--- tests/mut/frontend/conftest.py
+++ tests/mut/frontend/conftest.py
@@ -8,6 +8,7 @@ from unittest.mock import MagicMock
 
 import pytest
 
+from minegauler import utils
 from minegauler.core import api as core_api
 from minegauler.core import board
 from minegauler.core import utils as core_utils
@@ -16,7 +17,7 @@ from minegauler.core import utils as core_utils
 @pytest.fixture
 def ctrlr():
     mock = MagicMock()
-    mock.opts = core_utils.GameOptsStruct()
+    mock.opts = utils.GameOptsStruct()
     mock.board = board.Board(mock.opts.x_size, mock.opts.y_size)
     mock.mock_add_spec(core_api.AbstractSwitchingController, spec_set=True)
     return mock
