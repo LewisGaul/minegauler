f21e9a070c3ae253247accfd23332b79b33744ad
Lewis Gaul <lewis.gaul@gmail.com>
2019-12-30 18:09:12 +0000

    Add gracious Ctrl+C handling

--- minegauler/frontend/__init__.py
+++ minegauler/frontend/__init__.py
@@ -3,18 +3,21 @@ __init__.py - Available imports from the package
 
 December 2018, Lewis Gaul
 """
-
-__all__ = ("MinegaulerGUI", "init_app", "run_app", "state")
-
+import signal
 import sys
 
+from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QApplication
 
 from . import state
 from .main_window import MinegaulerGUI
 
 
+__all__ = ("MinegaulerGUI", "init_app", "run_app", "state")
+
+
 _app = None
+_timer = None
 
 
 def init_app() -> None:
@@ -31,9 +34,17 @@ def run_app(gui: MinegaulerGUI) -> int:
     :return:
         Exit code.
     """
+    global _timer
+
     if not _app:
-        raise RuntimeError("Must create the app before running the app")
+        raise RuntimeError("App must be initialised before being run")
 
     gui.show()
+
+    # Create a timer to run Python code periodically, so that Ctrl+C can be caught.
+    signal.signal(signal.SIGINT, lambda *args: gui.close())
+    _timer = QTimer()
+    _timer.timeout.connect(lambda: None)
+    _timer.start(100)
 
     return _app.exec_()
