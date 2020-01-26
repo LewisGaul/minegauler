8020385830bb5973e7af6f5939cf6d1e91658d4f
Lewis Gaul <lewis.gaul@gmail.com>
2020-01-26 23:07:24 +0000

    Add run.py entry point script

---
+++ run.py
@@ -0,0 +1,10 @@
+"""
+run.py - Entry point to the app
+
+January 2020, Lewis Gaul
+
+The app can be run with 'python -m minegauler' or just 'python run.py'.
+This file is provided primarily for use with pyinstaller.
+"""
+
+import minegauler.__main__
