484c911ca4ba9441b9de73fdd259b77e1a321d85
Lewis Gaul <lewis.gaul@gmail.com>
2022-01-09 22:02:14 +0000

    Fix benchmark IT

--- minegauler/core/__init__.py
+++ minegauler/core/__init__.py
@@ -1,6 +1,9 @@
 # April 2018, Lewis Gaul
 
-__all__ = ("UberController",)
+__all__ = ("BoardBase", "GameBase", "MinefieldBase", "UberController", "api", "regular")
 
-from . import api, board, controller, engine, game, minefield, regular
+from . import api, regular
+from .board import BoardBase
 from .engine import UberController
+from .game import GameBase
+from .minefield import MinefieldBase
