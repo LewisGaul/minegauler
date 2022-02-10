a566476afea0fa5e4d2d97e6ed8522ce453f403b
Lewis Gaul <lewis.gaul@gmail.com>
2022-02-10 00:22:26 +0000

    Fix highscores MUT

--- tests/mut/highscores_test.py
+++ tests/mut/highscores_test.py
@@ -62,31 +62,31 @@ class TestLocalHighscoreDatabase:
         my_hs = HighscoreStruct(
             GameMode.REGULAR, "M", 1, True, "Siwel G", 1234, 166.49, 322, 1.94, 0.0
         )
-        db.insert_highscore(my_hs)
+        db.insert_highscores([my_hs])
         highscores.append(my_hs)
         assert db.count_highscores() == 1
         assert db.get_highscores() == highscores
 
         # Multiple highscores
-        for hs in [
+        multiple_hs = [
             HighscoreStruct(
-                GameMode.REGULAR, "B", 1, False, "NAME1", 1234, 3.00, 5, 1.56, 0.0
+                GameMode.REGULAR, "B", 1, False, "NAME1", 123400, 3.00, 5, 1.56, 0.0
             ),
             HighscoreStruct(
-                GameMode.REGULAR, "B", 1, False, "NAME2", 1234, 3.11, 5, 1.56, 0.0
+                GameMode.REGULAR, "B", 1, False, "NAME2", 123401, 3.11, 5, 1.56, 0.0
             ),
             HighscoreStruct(
-                GameMode.REGULAR, "B", 1, True, "NAME1", 1234, 3.22, 5, 1.56, 0.0
+                GameMode.REGULAR, "B", 1, True, "NAME1", 123402, 3.22, 5, 1.56, 0.0
             ),
             HighscoreStruct(
-                GameMode.REGULAR, "B", 2, False, "NAME1", 1234, 3.33, 5, 1.56, 0.0
+                GameMode.REGULAR, "B", 2, False, "NAME1", 123403, 3.33, 5, 1.56, 0.0
             ),
             HighscoreStruct(
-                GameMode.REGULAR, "I", 1, False, "NAME1", 1234, 3.44, 5, 1.56, 0.0
+                GameMode.REGULAR, "I", 1, False, "NAME1", 123404, 3.44, 5, 1.56, 0.0
             ),
-        ]:
-            db.insert_highscore(hs)
-            highscores.append(hs)
+        ]
+        db.insert_highscores(multiple_hs)
+        highscores.extend(multiple_hs)
         highscores.sort(key=lambda h: h.elapsed)
         assert db.count_highscores() == len(highscores)
         assert db.get_highscores() == highscores
@@ -111,78 +111,12 @@ class TestLocalHighscoreDatabase:
             name="NAME1",
         ) == [
             HighscoreStruct(
-                GameMode.REGULAR, "B", 1, False, "NAME1", 1234, 3, 5, 1.56, 0
+                GameMode.REGULAR, "B", 1, False, "NAME1", 123400, 3, 5, 1.56, 0
             )
         ]
 
         # Case insensitive name match.
         assert db.get_highscores(name="SIWel g") == [my_hs]
-
-    def test_merge_db(self, tmpdir):
-        """Test merging DBs together."""
-        # Setup
-        base_db = SQLiteDB(tmpdir / "merge-base.db")
-        merge_db = SQLiteDB(tmpdir / "merge-from.db")
-        base_highscores = sorted(
-            [
-                HighscoreStruct(
-                    GameMode.REGULAR, "B", 1, False, "NAME1", 1234, 3.00, 5, 1.56, 0.0
-                ),
-                HighscoreStruct(
-                    GameMode.REGULAR, "B", 1, False, "NAME2", 1234, 3.11, 5, 1.56, 0.0
-                ),
-                HighscoreStruct(
-                    GameMode.REGULAR, "B", 1, True, "NAME1", 1234, 3.22, 5, 1.56, 0.0
-                ),
-                HighscoreStruct(
-                    GameMode.REGULAR, "B", 2, False, "NAME1", 1234, 3.33, 5, 1.56, 0.0
-                ),
-                HighscoreStruct(
-                    GameMode.REGULAR, "I", 1, False, "NAME1", 1234, 3.44, 5, 1.56, 0.0
-                ),
-            ],
-            key=lambda h: h.elapsed,
-        )
-        merge_highscores = sorted(
-            [
-                *base_highscores[:3],
-                HighscoreStruct(
-                    GameMode.REGULAR, "E", 3, True, "", 1234, 4.00, 5, 1.56, 0.0
-                ),
-                HighscoreStruct(
-                    GameMode.REGULAR, "E", 3, True, "blob", 1234, 4.01, 5, 1.56, 0.0
-                ),
-                HighscoreStruct(
-                    GameMode.REGULAR, "B", 1, False, "blob", 1234, 4.11, 5, 1.56, 0.0
-                ),
-            ],
-            key=lambda h: h.elapsed,
-        )
-        for hs in base_highscores:
-            base_db.insert_highscore(hs)
-        for hs in merge_highscores:
-            merge_db.insert_highscore(hs)
-
-        assert base_db.count_highscores() == len(base_highscores)
-        assert base_db.get_highscores() == base_highscores
-        assert merge_db.count_highscores() == len(merge_highscores)
-        assert merge_db.get_highscores() == merge_highscores
-
-        # Merge
-        base_db.merge_highscores(merge_db.path)
-        combined_highscores = sorted(
-            set(base_highscores) | set(merge_highscores), key=lambda h: h.elapsed
-        )
-        assert base_db.count_highscores() == len(combined_highscores)
-        assert base_db.get_highscores() == combined_highscores
-        assert merge_db.count_highscores() == len(merge_highscores)
-        assert merge_db.get_highscores() == merge_highscores
-
-    def test_merge_db_into_itself_error(self, tmp_local_db_path):
-        """Test error is raised when trying to merge DB into itself."""
-        db = SQLiteDB(tmp_local_db_path)
-        with pytest.raises(ValueError):
-            db.merge_highscores(db.path)
 
 
 class TestModuleAPIs:
