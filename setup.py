c28366014a8c6c1df6d98be6f3acf7e79d12147f
Lewis Gaul <lewis.gaul@gmail.com>
2022-02-21 17:59:50 +0000

    Revert move to pyproject.toml pep-621

--- setup.py
+++ setup.py
@@ -13,7 +13,30 @@ if "-" in version:
 
 
 setuptools.setup(
+    name="minegauler",
+    version=version,
+    author="Lewis Gaul",
+    author_email="minegauler@gmail.com",
+    description="A clone of the original minesweeper game with many added features",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LewisGaul/minegauler",
     packages=setuptools.find_packages(include="minegauler*"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: End Users/Desktop",
+        "Natural Language :: English",
+        "Topic :: Games/Entertainment :: Puzzle Games",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "attrs",
+        "PyQt5",
+        "requests",
+    ],
     package_data={
         "minegauler/app": [
             "boards/sample.mgb",
@@ -27,6 +50,11 @@ setuptools.setup(
         "minegauler/cli": [
             "cli.yaml",
         ],
+    },
+    project_urls={
+        "Bug Reports": "https://github.com/LewisGaul/minegauler/issues",
+        "Source": "https://github.com/LewisGaul/minegauler/",
+        "Background": "https://www.lewisgaul.co.uk/blog/coding/2020/02/12/minegauler/",
     },
     zip_safe=False,
 )
