3a49592c1638fafde027be3c736ffa0f1aca65b3
Lewis Gaul <lewis.gaul@gmail.com>
2025-01-13 12:39:19 +0000

    Ruff fixes in scripts/

--- scripts/create_release.py
+++ scripts/create_release.py
@@ -4,14 +4,15 @@ import argparse
 import enum
 import logging
 import os
-import pathlib
 import platform
 import shlex
 import shutil
 import subprocess
 import sys
 import tempfile
-from typing import Iterable, List, Optional
+from collections.abc import Iterable
+from pathlib import Path
+from typing import Optional, Self
 
 import minegauler.app  # To get the version
 
@@ -25,7 +26,7 @@ class Platform(enum.Enum):
     MACOS = enum.auto()
 
     @classmethod
-    def current(cls) -> "Platform":
+    def current(cls) -> Self:
         if sys.platform.startswith("win"):
             return cls.WINDOWS
         elif sys.platform.startswith("linux"):
@@ -41,7 +42,7 @@ class Format(enum.Enum):
     PLAIN = enum.auto()
 
     @classmethod
-    def from_str(cls, name: str) -> "Format":
+    def from_str(cls, name: str) -> Self:
         return getattr(cls, name.upper())
 
     @classmethod
@@ -58,7 +59,7 @@ class Format(enum.Enum):
             return f".{self.name.lower()}"
 
 
-def run_pyinstaller(output_dir: pathlib.Path) -> None:
+def run_pyinstaller(output_dir: Path) -> None:
     cmd = [
         # fmt: off
         "pyinstaller",
@@ -67,7 +68,7 @@ def run_pyinstaller(output_dir: pathlib.Path) -> None:
         # fmt: on
     ]
     logging.debug("Running command: %s", " ".join(shlex.quote(x) for x in cmd))
-    proc = subprocess.run(cmd)
+    proc = subprocess.run(cmd, check=False)
     if proc.returncode != 0:
         raise Exception(f"Pyinstaller command with exit code {proc.returncode}")
 
@@ -86,8 +87,8 @@ def create_windows_wrapper(
     run_cmd = "CALL" if console else "START"
     argv = [str(src), *argv]
     content.append(f"{run_cmd} {subprocess.list2cmdline(argv)} %*")
-    dest = pathlib.Path(dest).with_suffix(".bat")
-    with open(dest, "w") as f:
+    dest = Path(dest).with_suffix(".bat")
+    with open(dest, "w", encoding="utf-8") as f:
         f.write("\n".join(content))
 
 
@@ -108,8 +109,8 @@ def create_posix_wrapper(
     elif argv[0][0] != "/":
         argv[0] = "./" + argv[0]
     content.append(shlex.join(argv) + ' "$@"')
-    dest = pathlib.Path(dest).with_suffix(".sh")
-    with open(dest, "w") as f:
+    dest = Path(dest).with_suffix(".sh")
+    with open(dest, "w", encoding="utf-8") as f:
         f.write("\n".join(content))
     os.chmod(dest, 0o755)
 
@@ -128,7 +129,7 @@ def create_wrapper(
         create_posix_wrapper(src, dest, cd=cd, argv=argv, console=console)
 
 
-def create_package(from_dir: pathlib.Path, dest_dir: pathlib.Path, fmt: Format) -> str:
+def create_package(from_dir: Path, dest_dir: Path, fmt: Format) -> str:
     # Create outer files.
     shutil.copy2("CHANGELOG.md", from_dir / "CHANGELOG.txt")
     shutil.copy2("LICENSE.txt", from_dir)
@@ -147,11 +148,11 @@ def create_package(from_dir: pathlib.Path, dest_dir: pathlib.Path, fmt: Format) 
             os.path.join(APP_NAME, "minegauler-bot"), from_dir / "minegauler-bot"
         )
 
-    with open("package/README.txt.template", "r") as f:
+    with open("package/README.txt.template", "r", encoding="utf-8") as f:
         readme = f.read().format(
             root_filename=root_filename, dist_filename=dist_filename
         )
-    with open(from_dir / "README.txt", "w") as f:
+    with open(from_dir / "README.txt", "w", encoding="utf-8") as f:
         f.write(readme)
 
     # Copy over and archive at destination.
@@ -174,7 +175,7 @@ def archive_name() -> str:
     return f"minegauler-v{minegauler.app.__version__}-{plat}-{arch}"
 
 
-def parse_args(argv: Optional[List[str]] = None) -> argparse.Namespace:
+def parse_args(argv: Optional[list[str]] = None) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-f",
@@ -194,8 +195,8 @@ def parse_args(argv: Optional[List[str]] = None) -> argparse.Namespace:
         "-o",
         "--output-dir",
         metavar="DIR",
-        type=pathlib.Path,
-        default=pathlib.Path("./"),
+        type=Path,
+        default=Path("./"),
         help="output directory, defaults to cwd",
     )
     parser.add_argument(
@@ -204,13 +205,13 @@ def parse_args(argv: Optional[List[str]] = None) -> argparse.Namespace:
     return parser.parse_args(argv)
 
 
-def main(argv: List[str]) -> None:
+def main(argv: list[str]) -> None:
     args = parse_args(argv)
     logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
     with tempfile.TemporaryDirectory(prefix="pyinstaller-") as tmpdir:
-        tmpdir = pathlib.Path(tmpdir)
-        run_pyinstaller(tmpdir)
-        path = create_package(tmpdir, args.output_dir, args.format)
+        tmpdir_path = Path(tmpdir)
+        run_pyinstaller(tmpdir_path)
+        path = create_package(tmpdir_path, args.output_dir, args.format)
     logging.info("Created package at %s", path)
 
 
